908e5244a23bb71b20926cb664b6be42a948bafe
Rohit Singh <rohittsingh11@gmail.com>
2021-01-25 12:13:01 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Counter App with Flutter Bloc
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+
+## About <a name = "about"></a>
+
+The purpose of this project is to understand the bloc design pattern. In this project, we
+have made a simple counter app. This app has two buttons to increment and decrement the counter.
+In this project, we are using <a name="https://pub.dev/packages/flutter_bloc">flutter_bloc</a> library.
+
+## Getting Started <a name = "getting_started"></a>
+
+Anyone can clone it and run it without any hassle.
+
+### Prerequisites
+
+To run this project, you must have the up and running Flutter setup.
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running.
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_example/cubit/counter_cubit.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocBuilder<CounterCubit, CounterState>(
+              builder: (context, state) {
+                return Text(
+                  state.counterValue.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              BlocProvider.of<CounterCubit>(context).increment();
+              // context.read<CounterCubit>().increment();
+            },
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              // BlocProvider.of<CounterCubit>(context).decrement();
+              context.read<CounterCubit>().decrement();
+            },
+            tooltip: 'Decrement',
+            child: Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
